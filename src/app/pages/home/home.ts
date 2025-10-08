bfa9b0bec2c31a2c137d165fab6541198a7d178b
LuisaCastano40 <loise.0607@gmail.com>
2025-10-08 11:35:04 -0500

    [UPDATE] clase card

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component} from '@angular/core';
 // 1. Importar la clase del componente y agregarlo a los imports
-import { Navbar } from '../../components/navbar/navbar';
-
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Card],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })

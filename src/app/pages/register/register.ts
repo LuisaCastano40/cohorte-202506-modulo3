f4ae0994d87bdc31845c6a3c7786df7f1ee6738c
LuisaCastano40 <loise.0607@gmail.com>
2025-10-16 21:39:20 -0500

    [UPDATE] formularios

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+
+  registerForm = new FormGroup({
+    name : new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl<number | null >(null),
+    password: new FormControl<string>(''),
+  });
+
 
 }
